08a26a8391d43d7c1dbcc8a24cd4ea92107fb908
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2023-04-16 22:27:16 +0200

    clean

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,45 +1,26 @@
 #!/bin/sh
 
-mysql_install_db
-
-/etc/init.d/mysql start
-
 #Check if the database exists
-
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then 
 
 	echo "Database already exists"
 else
-
-# Set root option so that connexion without root password is not possible
-        echo "Securing mysql installation"
--S /bin/bash mysql_secure_installation <<_EOF_
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
+	mysql_install_db
+	/etc/init.d/mysql start
 
 #Add a root user on 127.0.0.1 to allow remote connexion
-
 	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Create database and user for wordpress
 	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;\
-        CREATE USER 'regular_user'@'%' IDENTIFIED BY 'password';\
         GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;\
-        GRANT SELECT, INSERT, UPDATE, DELETE ON $MYSQL_DATABASE.* TO 'regular_user'@'%';\
         " | mysql -uroot
 
 #Import database
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/savedb.sql
-
+	mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wp_save.sql
+	mysqladmin -u root password $MYSQL_ROOT_PASSWORD
+	/etc/init.d/mysql stop
 fi
-
-/etc/init.d/mysql stop
 
 exec "$@"
